f270d12e130ac1c69140404d67c3a3abed037607
kyungmi <purecolor85@gmail.com>
2017-08-15 15:02:29 +0900

    Add example of drawing simple graph

--- public/client.js
+++ public/client.js
@@ -1,4 +1,30 @@
-var stream = new EventSource("/sse");
+const MAX_DATA = 100;
+
+const stream = new EventSource(`/sse?limit=${MAX_DATA}`);
+const ctx = document.getElementById('chart').getContext('2d');
+const chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+  options: {
+    animation: false,
+    //Boolean - If we want to override with a hard coded scale
+    scaleOverride: true,
+    //** Required if scaleOverride is true **
+    //Number - The number of steps in a hard coded scale
+    scaleSteps: 10,
+    //Number - The value jump in the hard coded scale
+    scaleStepWidth: 10,
+    //Number - The scale starting value
+    scaleStartValue: 0
+  },
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'value',
+      data: [],
+    }]
+  }
+});
 
 stream.onopen = function() {
   log('Opened connection 🎉');
@@ -9,17 +35,32 @@ stream.onerror = function (event) {
 };
 
 stream.onmessage = function (event) {
+  const serverData = JSON.parse(event.data);
+
+  const data = chart.data.datasets[0].data;
+  const labels = chart.data.labels;
+
+  serverData.forEach((newData) => {
+    data.push(newData.value);
+    labels.push(newData.timestamp);
+  });
+
+  if (data.length > MAX_DATA) {
+    data.splice(0, data.length - MAX_DATA);
+    labels.shift();
+  }
+  chart.update();
   log('Received Message: ' + event.data);
 };
 
-document.querySelector('#close').addEventListener('click', function(event) {
+document.querySelector('#close').addEventListener('click', () => {
   stream.close();
   log('Closed connection 😱');
 });
 
-var list = document.getElementById('log');
-var log = function(text) {
-  var li = document.createElement('li');
+const list = document.getElementById('log');
+const log = function(text) {
+  const li = document.createElement('li');
   li.innerHTML = text;
   list.appendChild(li);
 }
@@ -30,10 +71,10 @@ window.addEventListener('beforeunload', function() {
 
 // can still push to the server as usual with good old ajax
 
-document.querySelector('#send').addEventListener('click', function(event) {
-  var json = JSON.stringify({ message: 'Hey there' });
+document.querySelector('#send').addEventListener('click', () => {
+  const json = JSON.stringify({ message: 'Hey there' });
 
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('POST', '/api', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(json);
--- server.js
+++ server.js
@@ -1,53 +1,58 @@
+const http = require('http');
+const express = require('express');
+const SSE = require('sse');
+const data = require('./data');
 
+const app = express().use(express.static('public'));
+const server = http.createServer(app);
+const clients = [];
 
-var http = require('http');
-var express = require('express');
-var SSE = require('sse');
+server.listen(8080, '127.0.0.1', () => {
+  const sse = new SSE(server);
 
-var app = express().use(express.static('public'));
-var server = http.createServer(app);
-var clients = [];
-
-server.listen(8080, '127.0.0.1', function() {
-  var sse = new SSE(server);
-
-  sse.on('connection', function(stream) {
-    clients.push(stream);
+  sse.on('connection', function(client, query) {
+    clients.push(client);
     console.log('Opened connection 🎉');
 
-    var json = JSON.stringify({ message: 'Gotcha' });
-    stream.send(json);
+    let limit = 100;
+    if (query && query.limit && !isNaN(query.limit)) {
+      limit = parseInt(query.limit, 10);
+    }
+
+    const json = JSON.stringify(data.get(limit)); // Initial data
+    client.send(json);
     console.log('Sent: ' + json);
 
-    stream.on('close', function() {
-      clients.splice(clients.indexOf(stream), 1);
+    client.on('close', function() {
+      clients.splice(clients.indexOf(client), 1);
       console.log('Closed connection 😱');
     });
   });
 });
 
-var broadcast = function() {
-  var json = JSON.stringify({ message: 'Hello hello!' });
+
+const broadcast = () => {
+  const json = JSON.stringify(data.get());
 
   clients.forEach(function(stream) {
     stream.send(json);
     console.log('Sent: ' + json);
   });
-}
-setInterval(broadcast, 3000)
+};
+setInterval(broadcast, 500);
 
 // can receive from the client with standard http and broadcast
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.post('/api', function(req, res) {
-  var message = JSON.stringify(req.body);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.post('/api', (req, res) => {
+  const message = JSON.stringify(req.body);
   console.log('Received: ' + message);
   res.status(200).end();
 
-  var json = JSON.stringify({ message: 'Something changed' });
+  const json = JSON.stringify({ message: 'Something changed' });
   clients.forEach(function(stream) {
     stream.send(json);
     console.log('Sent: ' + json);
   });
-})
+});
